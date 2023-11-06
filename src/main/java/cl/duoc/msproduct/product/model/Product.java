a339f41e80b6d3b0419c59e889e8b3d74df95220
ThonyFlores <anthony.g91@icloud.com>
2023-11-06 01:19:06 -0300

    headers crosOrigin

--- src/main/java/cl/duoc/msproduct/product/model/Product.java
+++ src/main/java/cl/duoc/msproduct/product/model/Product.java
@@ -1,16 +1,16 @@
 package cl.duoc.msproduct.product.model;
 
-import java.util.Date;
+// import java.util.Date;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
+// import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
+// import jakarta.persistence.Temporal;
+// import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -26,16 +26,17 @@ public class Product {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	private String description;
 	private float price;
 	@Column(name = "url_image")
 	private String urlImage;	
 	private boolean active;
-	@Column(name="create_at")
-	@Temporal(TemporalType.DATE)
-	private Date createAt;
+	// @Column(name="create_at")
+	// @Temporal(TemporalType.DATE)
+	// private Date createAt;
 
-	@PrePersist //antes de persistir la info en la bd se crea la fecha
-	public void prePersist() {
-		createAt = new Date();
-	}
+	// @PrePersist //antes de persistir la info en la bd se crea la fecha
+	// public void prePersist() {
+	// 	createAt = new Date();
+	// }
 }
